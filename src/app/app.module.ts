c12fccf6166b793bb0892608f4a46559f9ffa77d
Karin Karlsson <karin.drake@gmail.com>
2020-02-27 11:54:10 +0100

    Initierade projekt samt skapade components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DialogueComponent } from './dialogue/dialogue.component';
+import { SelectBeverageComponent } from './select-beverage/select-beverage.component';
+import { TheUsualComponent } from './the-usual/the-usual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogueComponent,
+    SelectBeverageComponent,
+    TheUsualComponent
   ],
   imports: [
     BrowserModule
